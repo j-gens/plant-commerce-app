cb8678734a21ca369d3e01468c52316747f61bf8
j-gens <jgens2@gmail.com>
2020-02-07 10:23:17 -0800

    boilerplate express server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = require('./app.js');
+require('dotenv').config();
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3100;
+}
+
+app.listen(port, () => console.log(`pug commerce is listening on ${port}!`))
