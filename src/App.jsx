cf66643573d02d46b358e4f4249c19637f0f5e23
j-gens <jgens2@gmail.com>
2020-02-07 10:32:06 -0800

    Add boilerplate App component and style sheet

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        hello world
+      </div>
+    )
+  }
+}
+
+export default App;
