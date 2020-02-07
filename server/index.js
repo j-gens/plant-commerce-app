decac010bf9fd92aa414094f73850c527ac40a16
j-gens <jgens2@gmail.com>
2020-02-07 15:55:01 -0800

    Update syntax for eslint

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
 const app = require('./app.js');
+
 require('dotenv').config();
 
 let port = process.env.PORT;
-if (port == null || port == "") {
+if (port === null || port === '') {
   port = 3100;
 }
 
-app.listen(port, () => console.log(`pug commerce is listening on ${port}!`))
+app.listen(port, () => console.log(`pug commerce is listening on ${port}!`));
