98f60f12bbecd5b778f3161125bc894ec8c06c14
j-gens <jgens2@gmail.com>
2020-02-09 12:27:09 -0800

    Deconstruct props and add additional class for background image

--- src/components/home-menu-item/home-menu-item.component.jsx
+++ src/components/home-menu-item/home-menu-item.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './home-menu-item.styles.css'
 
-const HomeMenuItem = (props) => (
-  <div className="home-menu-item">
+const HomeMenuItem = ({ title, size, imageUrl }) => (
+  <div className={`home-menu-item ${size}`}>
+    <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}></div>
     <div className="content">
-      <h1 className="title">{props.menuItem}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
