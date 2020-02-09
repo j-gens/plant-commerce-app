7553ce2f8f8740d4f85363c6d9a6fc1d37daa532
j-gens <jgens2@gmail.com>
2020-02-08 16:05:55 -0800

    Add some styling elements for App

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="homepage">
         <HomeMenu homeMenuList={this.state.homeMenuList} />
       </div>
     )
