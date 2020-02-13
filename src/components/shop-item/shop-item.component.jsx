c1c0153cdf65a38f2a021ae865ef51f3f60e6eea
j-gens <jgens2@gmail.com>
2020-02-13 11:00:43 -0800

    Add functional component and styles for shop item

---
+++ src/components/shop-item/shop-item.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './shop-item.styles.css';
+
+
+const ShopItem = ({ id, name, imageUrl, price }) => (
+  <div className="shop-item-bin">
+    <div className="shop-item-image" style={{backgroundImage: `url(${imageUrl})`}}></div>
+    <div className="shop-item-text">
+      <span className="item-name">{name}</span>
+      <span className="item-price">{price}</span>
+    </div>
+  </div>
+);
+
+
+export default ShopItem;
