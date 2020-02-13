41348fc6002b513886f97d415af825ef287a01aa
j-gens <jgens2@gmail.com>
2020-02-13 13:02:46 -0800

    Update item name to all uppercase

--- src/components/shop-item/shop-item.component.jsx
+++ src/components/shop-item/shop-item.component.jsx
@@ -7,7 +7,7 @@ const ShopItem = ({ id, name, imageUrl, price }) => (
   <div className="shop-item-bin">
     <div className="shop-item-image" style={{backgroundImage: `url(${imageUrl})`}}></div>
     <div className="shop-item-text">
-      <span className="item-name">{name}</span>
+      <span className="item-name">{name.toUpperCase()}</span>
       <span className="item-price">${price}</span>
     </div>
   </div>
