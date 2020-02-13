058d3c08070afbc37b9011b928dc8ed838442970
j-gens <jgens2@gmail.com>
2020-02-13 13:20:22 -0800

    Import in header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Header from './components/header/header.component.jsx';
 import HomeMenu from './components/home-menu/home-menu.component.jsx';
 import ShopMenu from './components/shop-menu/shop-menu.component.jsx';
 import './App.styles.css';
@@ -8,6 +9,7 @@ import './App.styles.css';
 
 const App = () => (
   <div className="homepage">
+    <Header />
     <Switch>
       <Route exact path="/" component={HomeMenu} />
       <Route path="/shop" component={ShopMenu} />
