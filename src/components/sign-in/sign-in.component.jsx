b344c3922bcb85fbf3b390df56907e23a714f431
j-gens <jgens2@gmail.com>
2020-02-16 09:42:47 -0800

    Boilerplate class component and style sheet

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './sign-in.styles.css';
+
+
+class SignIn extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        heeeeellllloooo
+      </div>
+    );
+  }
+}
+
+
+export default SignIn;
