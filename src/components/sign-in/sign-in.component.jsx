b5e8e4a86d05ea3dcf6efede5c7e3a32a6cb6a32
j-gens <jgens2@gmail.com>
2020-02-16 09:53:39 -0800

    Add basic form for email and password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,17 +4,29 @@ import './sign-in.styles.css';
 
 
 class SignIn extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-
+      email: '',
+      password: ''
     }
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
   }
 
   render() {
     return (
-      <div>
-        heeeeellllloooo
+      <div className="form">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+        <form>
+          <input required name="email" value={this.state.email} onChange={this.handleChange}></input>
+          <label>Email</label>
+          <input required name="password" value={this.state.password} onChange={this.handleChange}></input>
+          <label>Password</label>
+        </form>
       </div>
     );
   }
