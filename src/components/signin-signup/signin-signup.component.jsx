1e89e34da5e8079b13cced3c9ded1b3f8ef3f7e4
j-gens <jgens2@gmail.com>
2020-02-16 09:53:10 -0800

    Import SignIn component

--- src/components/signin-signup/signin-signup.component.jsx
+++ src/components/signin-signup/signin-signup.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import SignIn from '../sign-in/sign-in.component.jsx';
 import './signin-signup.styles.css';
 
 
 const SignInSignUp = () => (
   <div>
-    hello
+    <SignIn />
   </div>
 );
 
