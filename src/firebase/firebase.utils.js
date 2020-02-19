4b4eeba83adc7ba43a6562f351bbb97a7a6597e0
j-gens <jgens2@gmail.com>
2020-02-19 14:07:36 -0800

    Correct typo with function call

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -24,7 +24,7 @@ export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => firebase.auth.signInWithPopup(provider);
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 
 export default firebase;
