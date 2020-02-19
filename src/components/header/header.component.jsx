70fc290dad805c8e3e19f9cab62f77cd7216a8bc
j-gens <jgens2@gmail.com>
2020-02-19 15:15:58 -0800

    Dynamically change sign in/sign out options based on currentUser

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { auth } from '../../firebase/firebase.utils.js';
+
 import './header.styles.css';
 
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className="header-bin">
     <div className="header-logo">
       <Link to="/">LOGO</Link>
     </div>
-    <div className="header-text">
-      <Link to="/shop">SHOP</Link>
-      <Link to="/contact">CONTACT</Link>
+    <div className="header-options">
+      <Link className="option" to="/shop">SHOP</Link>
+      <Link className="option" to="/contact">CONTACT</Link>
+      {
+        currentUser ?
+        <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+        :
+        <Link className="option" to="/signin">SIGN IN</Link>
+      }
     </div>
   </div>
 );
