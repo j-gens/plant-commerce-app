9d468c228dab62c78e7bcb1c45b8eb01f60b3b4f
j-gens <jgens2@gmail.com>
2020-02-19 14:52:57 -0800

    Change App to a stateful component to track current user

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,44 @@ import Header from './components/header/header.component.jsx';
 import HomeMenu from './components/home-menu/home-menu.component.jsx';
 import ShopMenu from './components/shop-menu/shop-menu.component.jsx';
 import SignInSignUp from './components/signin-signup/signin-signup.component.jsx';
+import { auth } from './firebase/firebase.utils.js';
+
 import './App.styles.css';
 
 
-const App = () => (
-  <div className="homepage">
-    <Header />
-    <Switch>
-      <Route exact path="/" component={HomeMenu} />
-      <Route path="/shop" component={ShopMenu} />
-      <Route path="/signin" component={SignInSignUp} />
-    </Switch>
-  </div>
-)
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>
+      this.setState({ currentUser: user });
+    );
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div className="homepage">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomeMenu} />
+          <Route path="/shop" component={ShopMenu} />
+          <Route path="/signin" component={SignInSignUp} />
+        </Switch>
+      </div>
+    );
+  }
+}
 
 
 export default App;
