8a715e2c30ee76438e025e3fbd6c2111d6d92bb2
j-gens <jgens2@gmail.com>
2020-02-24 13:59:44 -0800

    Update sign-up to boilerplate  stateful component

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component.jsx';
+import FormButton from '../form-button/form-button.component.jsx';
+import { auth, createUserProfileDoc } from '../../firebase/firebase.utils.js';
+
 import './sign-up.styles.css';
 
 
-const SignUp = () => (
-  <div>
-    sign in here
-  </div>
-);
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    )
+  }
+}
 
 
 export default SignUp;
