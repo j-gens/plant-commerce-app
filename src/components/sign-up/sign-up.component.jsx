c85902ebd7ca45866007cce717972e3fb089bf3c
j-gens <jgens2@gmail.com>
2020-02-24 14:23:38 -0800

    Add basic form elements for user to sign up

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -11,16 +11,60 @@ class SignUp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      displayName: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
     }
+  }
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
   }
 
   render() {
     return (
-      <div>
-        hello
+      <div className="sign-up">
+        <h2>I need to create an account</h2>
+        <span>sign up with an email and password</span>
+        <FormInput
+          name="displayName"
+          type="name"
+          value={this.state.displayName}
+          handleChange={this.handleChange}
+          label="Display Name"
+          required
+        />
+        <FormInput
+          name="email"
+          type="email"
+          value={this.state.email}
+          handleChange={this.handleChange}
+          label="Email"
+          required
+        />
+        <FormInput
+          name="password"
+          type="password"
+          value={this.state.password}
+          handleChange={this.handleChange}
+          label="Password"
+          required
+        />
+        <FormInput
+          name="confirmPassword"
+          type="password"
+          value={this.state.confirmPassword}
+          handleChange={this.handleChange}
+          label="Confirm Password"
+          required
+        />
+        <FormButton type="submit">
+          sign up
+        </FormButton>
       </div>
-    )
+    );
   }
 }
 
