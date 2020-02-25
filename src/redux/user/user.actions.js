75efb01fcb6c91701c3aecc6cdb6d3173090b455
j-gens <jgens2@gmail.com>
2020-02-25 15:49:56 -0800

    Import UserActionTypes to replace string for action.type

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,7 +1,7 @@
-const setCurrentUser = (user) => ({
-  type: 'SET_CURRENT_USER',
+import { UserActionTypes } from './user.types';
+
+
+export const setCurrentUser = (user) => ({
+  type: UserActionTypes.SET_CURRENT_USER,
   payload: user,
 });
-
-
-export default setCurrentUser;
