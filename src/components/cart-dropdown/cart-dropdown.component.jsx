0045728ca3c45a5fa09390fc251d8e10759296e1
j-gens <jgens2@gmail.com>
2020-02-25 17:03:01 -0800

    Add additional classNames and button element

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
+import FormButton from '../form-button/form-button.component.jsx';
 import './cart-dropdown.styles.css';
 
 
-const Cart = () => (
-  <div>
+const CartDropdown = () => (
+  <div className="cart-dropdown">
+    <div className="cart-items">
 
+    </div>
+    <FormButton>
+      checkout
+    </FormButton>
   </div>
 );
 
 
-export default Cart;
+export default CartDropdown;
