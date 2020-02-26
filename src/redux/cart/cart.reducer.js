ae1505bf9a0196d6447909c007cd472436aa0f34
j-gens <jgens2@gmail.com>
2020-02-26 12:14:52 -0800

    Add initial state and case for adding items to cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -3,6 +3,7 @@ import CartActionTypes from './cart.types';
 
 const INITIAL_STATE = {
   isHidden: true,
+  cartItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -12,6 +13,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         isHidden: !state.isHidden,
       };
+      case CartActionTypes.ADD_ITEM:
+        return {
+          ...state,
+          cartItems: [...state.cartItems, action.payload],
+        };
     default:
       return state;
   }
