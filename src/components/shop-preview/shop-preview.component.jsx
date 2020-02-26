f33ae9b1201c5ec30ae9f8743cb168a56d7b0d27
j-gens <jgens2@gmail.com>
2020-02-26 12:28:17 -0800

    Pass entire item object as props to child components

--- src/components/shop-preview/shop-preview.component.jsx
+++ src/components/shop-preview/shop-preview.component.jsx
@@ -11,8 +11,8 @@ const ShopPreview = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item, index) => index < 4)
-        .map(({ id, ...otherItemProps }) =>
-        <ShopItem key={id} { ...otherItemProps } />
+        .map((item) =>
+        <ShopItem key={item.id} item={item} />
       )}
     </div>
   </div>
