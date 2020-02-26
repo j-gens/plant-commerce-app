d61cae7c23155743d8dab11f472e03da4ed04acb
j-gens <jgens2@gmail.com>
2020-02-26 15:17:28 -0800

    Add reduce method to calculate cart item quantities

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,15 +5,19 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
 import './cart-icon.styles.css';
 
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, totalCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
-    <span className="cart">CART(0)</span>
+    <span className="cart">CART({totalCount})</span>
   </div>
 );
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  totalCount: cartItems.reduce((total, item) => total + item.quantity, 0),
+})
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
