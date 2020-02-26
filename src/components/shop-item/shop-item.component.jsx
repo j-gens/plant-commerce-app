9f8c91dcf94e75f21be203b8921ca008e834e867
j-gens <jgens2@gmail.com>
2020-02-26 12:28:46 -0800

    Set up mapDispatchToProps to add item(s) to cart

--- src/components/shop-item/shop-item.component.jsx
+++ src/components/shop-item/shop-item.component.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import FormButton from '../form-button/form-button.component.jsx';
+import { addCartItem } from '../../redux/cart/cart.actions.js';
 import './shop-item.styles.css';
 
 
-const ShopItem = ({ id, name, imageUrl, price }) => (
-  <div className="shop-item-bin">
-    <div className="shop-item-image" style={{backgroundImage: `url(${imageUrl})`}}></div>
-    <div className="shop-item-text">
-      <span className="item-name">{name.toUpperCase()}</span>
-      <span className="item-price">${price}</span>
+const ShopItem = ({ item, addCartItem }) => {
+  const { imageUrl, name, price } = item;
+  return (
+    <div className="shop-item-bin">
+      <div className="shop-item-image" style={{backgroundImage: `url(${imageUrl})`}}></div>
+      <div className="shop-item-text">
+        <span className="item-name">{name.toUpperCase()}</span>
+        <span className="item-price">${price}</span>
+      </div>
+      <FormButton inverted onClick={() => addCartItem(item)}>add to cart</FormButton>
     </div>
-    <FormButton inverted>add to cart</FormButton>
-  </div>
-);
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  addCartItem: (item) => dispatch(addCartItem(item)),
+});
 
 
-export default ShopItem;
+export default connect(null, mapDispatchToProps)(ShopItem);
