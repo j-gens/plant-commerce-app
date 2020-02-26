b2e1c5e45ed435a55f550fc582dbabcf0059e860
j-gens <jgens2@gmail.com>
2020-02-26 11:00:11 -0800

    Add inverted prop to add className for styling

--- src/components/form-button/form-button.component.jsx
+++ src/components/form-button/form-button.component.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import './form-button.styles.css';
 
 
-const FormButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-  <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} form-button`} {...otherProps} >
+const FormButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+  <button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} form-button`} {...otherProps} >
     {children.toUpperCase()}
   </button>
 );
