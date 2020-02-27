69f8a4f6fdd20b887a26fec6792c2ab9b076685b
j-gens <jgens2@gmail.com>
2020-02-27 12:21:47 -0800

    Boilerplate functional component and styles sheet

---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './checkout-item.styles.css';
+
+
+const CheckoutItem = () => (
+  <div className="checkout-item-bin">
+    checkout item
+  </div>
+);
+
+
+export default CheckoutItem;
