1d08add2c9ad91b2877dfcdc8c73e62da7401fbb
j-gens <jgens2@gmail.com>
2020-02-27 11:37:08 -0800

    Build out header section of checkout page

--- src/components/checkout/checkout.component.jsx
+++ src/components/checkout/checkout.component.jsx
@@ -4,8 +4,24 @@ import './checkout.styles.css';
 
 
 const Checkout = () => (
-  <div>
-    checkout page
+  <div className="checkout-bin">
+    <div className="checkout-header">
+      <div className="header-text">
+        <span>Product</span>
+      </div>
+      <div className="header-text">
+        <span>Description</span>
+      </div>
+      <div className="header-text">
+        <span>Quantity</span>
+      </div>
+      <div className="header-text">
+        <span>Price</span>
+      </div>
+      <div className="header-text">
+        <span>Remove</span>
+      </div>
+    </div>
   </div>
 );
 
