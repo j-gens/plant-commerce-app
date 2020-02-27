de76a748f088ab1fec10e1e8ec8f499a3c7e200f
j-gens <jgens2@gmail.com>
2020-02-27 14:30:12 -0800

    Fill out divs for item details

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -3,9 +3,23 @@ import React from 'react';
 import './checkout-item.styles.css';
 
 
-const CheckoutItem = () => (
+const CheckoutItem = ({ item: { imageUrl, name, price, quantity }}) => (
   <div className="checkout-item-bin">
-    checkout item
+    <div className="ch-item-block">
+      <img src={imageUrl} alt={name} />
+    </div>
+    <div className="ch-item-block">
+      <span>{name}</span>
+    </div>
+    <div className="ch-item-block">
+      <span>{quantity}</span>
+    </div>
+    <div className="ch-item-block">
+      <span>${price * quantity}</span>
+    </div>
+    <div className="ch-item-block">
+      <span>&#10005;</span>
+    </div>
   </div>
 );
 
