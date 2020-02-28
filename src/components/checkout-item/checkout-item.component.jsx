5b9297cff6d62cb575ccc8ae6b911e59ccee69a7
j-gens <jgens2@gmail.com>
2020-02-27 17:04:19 -0800

    Set up redux dispatch for clearCartItem action

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,27 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { clearCartItem } from '../../redux/cart/cart.actions.js';
 
 import './checkout-item.styles.css';
 
 
-const CheckoutItem = ({ item: { imageUrl, name, price, quantity }}) => (
-  <div className="checkout-item-bin">
-    <div className="ch-item-block">
-      <img src={imageUrl} alt={name} />
+const CheckoutItem = ({ item, clearCartItem }) => {
+  const { imageUrl, name, price, quantity } = item;
+  return (
+    <div className="checkout-item-bin">
+      <div className="ch-item-block">
+        <img src={imageUrl} alt={name} />
+      </div>
+      <div className="ch-item-block">
+        <span>{name}</span>
+      </div>
+      <div className="ch-item-block">
+        <span>{quantity}</span>
+      </div>
+      <div className="ch-item-block">
+        <span>${price}</span>
+      </div>
+      <div className="ch-item-block">
+        <span className="ch-remove" onClick={() => clearCartItem(item)}>&#10005;</span>
+      </div>
     </div>
-    <div className="ch-item-block">
-      <span>{name}</span>
-    </div>
-    <div className="ch-item-block">
-      <span>{quantity}</span>
-    </div>
-    <div className="ch-item-block">
-      <span>${price * quantity}</span>
-    </div>
-    <div className="ch-item-block">
-      <span>&#10005;</span>
-    </div>
-  </div>
-);
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  clearCartItem: (item) => dispatch(clearCartItem(item)),
+});
 
 
-export default CheckoutItem;
+export default connect(null, mapDispatchToProps)(CheckoutItem);
