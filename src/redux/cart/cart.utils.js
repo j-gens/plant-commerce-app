8a92e75de0cf8bb1f5c5ab4c8c06e66392c63941
j-gens <jgens2@gmail.com>
2020-02-28 11:00:00 -0800

    Create utility function for reducing item quantity in cart

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -9,3 +9,18 @@ export const addItemToCart = (cartItems, newItem) => {
 
   return [...cartItems, { ...newItem, quantity: 1 }];
 };
+
+
+export const removeItemFromCart = (cartItems, removeItem) => {
+  const isExistingCartItem = cartItems.find((cartItem) => cartItem.id === removeItem.id);
+
+  if (isExistingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) =>
+      cartItem.id !== removeItem.id
+    );
+  }
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === removeItem.id ? { ...cartItem, quantity: cartItem.quantity - 1 } : cartItem
+  );
+};
