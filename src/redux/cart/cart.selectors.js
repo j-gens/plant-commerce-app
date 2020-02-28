5a78296ba92769b14272101133b5287ebcdc1e74
j-gens <jgens2@gmail.com>
2020-02-28 10:59:40 -0800

    Add trailing commas for linting rule

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -5,20 +5,20 @@ const cartSelector = (state) => state.cart;
 
 export const cartItemsSelector = createSelector(
   [cartSelector],
-  (cart) => cart.cartItems
+  (cart) => cart.cartItems,
 );
 
 export const hiddenCartSelector = createSelector(
   [cartSelector],
-  (cart) => cart.isHidden
+  (cart) => cart.isHidden,
 );
 
 export const cartItemCountSelector = createSelector(
   [cartItemsSelector],
-  (cartItems) => cartItems.reduce((total, item) => total + item.quantity, 0)
+  (cartItems) => cartItems.reduce((total, item) => total + item.quantity, 0),
 );
 
 export const cartTotalCostSelector = createSelector(
   [cartItemsSelector],
-  (cartItems) => cartItems.reduce((total, item) => total + (item.price * item.quantity), 0)
+  (cartItems) => cartItems.reduce((total, item) => total + (item.price * item.quantity), 0),
 );
