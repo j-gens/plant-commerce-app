1edb588297684f7f0228f7423c616aeaa9b15b93
j-gens <jgens2@gmail.com>
2020-02-29 15:59:01 -0800

    Move homeMenu state into redux for testing purposes

---
+++ src/redux/home-menu/home-menu.reducer.js
@@ -0,0 +1,19 @@
+const INITIAL_STATE = {
+  homeMenu: [
+    {id: 1, title: 'hats', imageUrl: 'pug-background.jpg' , size: '', linkUrl: 'hats'},
+    {id: 2, title: 'jackets', imageUrl: 'pug-background.jpg', size: '', linkUrl: 'jackets'},
+    {id: 3, title: 'sneakers', imageUrl: 'pug-background.jpg', size: '', linkUrl: 'sneakers'},
+    {id: 4, title: 'womens', imageUrl: 'pug-background.jpg', size: 'large', linkUrl: 'womens'},
+    {id: 5, title: 'mens', imageUrl: 'pug-background.jpg', size: 'large', linkUrl: 'mens'}
+  ],
+};
+
+const homeMenuReducer = (state = INITIAL_STATE, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+
+
+export default homeMenuReducer;
