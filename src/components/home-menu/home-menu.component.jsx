5599cc6c2bb51587cf962a71d96b0748b250380d
j-gens <jgens2@gmail.com>
2020-02-29 16:28:30 -0800

    Change to functional component and utilize reselect

--- src/components/home-menu/home-menu.component.jsx
+++ src/components/home-menu/home-menu.component.jsx
@@ -1,34 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import HomeMenuItem from '../home-menu-item/home-menu-item.component.jsx'
+import HomeMenuItem from '../home-menu-item/home-menu-item.component.jsx';
+import { sectionsSelector } from '../../redux/home-menu/home-menu.selectors.js';
 
-import './home-menu.styles.css'
-
-
-class HomeMenu extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      homeMenu: [
-        {id: 1, title: 'hats', imageUrl: 'pug-background.jpg' , size: '', linkUrl: 'hats'},
-        {id: 2, title: 'jackets', imageUrl: 'pug-background.jpg', size: '', linkUrl: 'jackets'},
-        {id: 3, title: 'sneakers', imageUrl: 'pug-background.jpg', size: '', linkUrl: 'sneakers'},
-        {id: 4, title: 'womens', imageUrl: 'pug-background.jpg', size: 'large', linkUrl: 'womens'},
-        {id: 5, title: 'mens', imageUrl: 'pug-background.jpg', size: 'large', linkUrl: 'mens'}
-      ]
-    }
-  }
-
-  render() {
-    return(
-      <div className="home-menu">
-        {this.state.homeMenu.map(({ id, ...otherHomeMenuProps }) =>
-          <HomeMenuItem key={id} {...otherHomeMenuProps} />
-        )}
-      </div>
-    )
-  }
-}
+import './home-menu.styles.css';
 
 
-export default HomeMenu
+const HomeMenu = ({ sections }) => (
+  <div className="home-menu">
+    {sections.map(({ id, ...otherSectionProps }) =>
+      <HomeMenuItem key={id} {...otherSectionProps} />
+    )}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  sections: sectionsSelector,
+})
+
+export default connect(mapStateToProps)(HomeMenu);
