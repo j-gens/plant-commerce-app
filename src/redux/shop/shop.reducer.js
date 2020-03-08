d679f7bce163e4a4dfb555e5ea44433624009c06
j-gens <jgens2@gmail.com>
2020-03-08 09:51:41 -0700

    Boilerplate reducer and selectors for shop data

---
+++ src/redux/shop/shop.reducer.js
@@ -0,0 +1,13 @@
+import SHOP_DATA from '../../data/shopData.js';
+
+
+const INITIAL_STATE = {
+  shopData: SHOP_DATA,
+};
+
+export default shopReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
