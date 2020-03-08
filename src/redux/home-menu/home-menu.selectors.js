466e43972159120da89903e4be01ad8d5055de7a
j-gens <jgens2@gmail.com>
2020-03-08 09:52:41 -0700

    Update spacing to be consistent across files

--- src/redux/home-menu/home-menu.selectors.js
+++ src/redux/home-menu/home-menu.selectors.js
@@ -1,5 +1,6 @@
 import { createSelector } from 'reselect';
 
+
 const homeMenuSelector = (state) => state.homeMenu;
 
 export const sectionsSelector = createSelector(
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,6 +5,7 @@ import storage from 'redux-persist/lib/storage';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 import homeMenuReducer from './home-menu/home-menu.reducer';
+import shopReducer from './shop/shop.reducer';
 
 
 const persistConfig = {
@@ -17,6 +18,7 @@ const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
   homeMenu: homeMenuReducer,
+  shop: shopReducer,
 });
 
 
