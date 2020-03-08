77506dc254ff68a0e69fe5482e3635b03637ac2a
j-gens <jgens2@gmail.com>
2020-03-08 11:39:12 -0700

    Set up routing between menu and category pages

--- src/components/shop-page/shop-page.component.jsx
+++ src/components/shop-page/shop-page.component.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
+import ShopMenu from '../shop-menu/shop-menu.component.jsx';
+import ShopCategory from '../shop-category/shop-category.component.jsx';
 
 import './shop-page.styles.css';
 
 
-const ShopPage = () => (
+const ShopPage = ({ match }) => (
   <div>
-
+    <Route exact path={`${match.path}`} component={ShopMenu} />
+    <Route path={`${match.path}/:categoryId`} component={ShopCategory} />
   </div>
 );
 
 
-export default shopPage;
+export default ShopPage;
