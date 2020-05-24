a5e9c9ae4e45421212dddf66a21d614282435df8
j-gens <jgens2@gmail.com>
2020-05-24 11:42:09 -0700

    Add details for processing stripe payments

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 
 import './stripe-button.styles.css';
 
 
-const StripeButton = () => (
-  <div>
+const StripeButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publicKey = 'pk_test_TiscNZGKDRCBI5UZN244V2OB00FuezS7ND';
+  const onToken = (token) => {
+    console.log(token);
+  }
 
-  </div>
-);
+  return (
+    <StripeCheckout
+      name='LOGO'
+      description={`your total is $${price}`}
+      image=''
+      panelLabel='Pay Now'
+      amount={priceForStripe}
+      stripeKey={publicKey}
+      shippingAddress
+      billingAddress
+      token={onToken}
+    />
+  );
+};
 
 
 export default StripeButton;
