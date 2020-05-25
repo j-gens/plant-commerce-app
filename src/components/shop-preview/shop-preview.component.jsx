47fad8ab80b74e126024c1f3480d700bda0825dc
j-gens <jgens2@gmail.com>
2020-05-25 07:46:47 -0700

    Add link to shop preview title for nested routing to each shop section

--- src/components/shop-preview/shop-preview.component.jsx
+++ src/components/shop-preview/shop-preview.component.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 import ShopItem from '../shop-item/shop-item.component.jsx';
 
 import './shop-preview.styles.css';
 
 
-const ShopPreview = ({ title, items }) => (
-  <div className="preview-bin">
-    <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview">
-      {items
-        .filter((item, index) => index < 4)
-        .map((item) =>
-          <ShopItem key={item.id} item={item} />
-      )}
+const ShopPreview = ({ title, items }) => {
+  const { url } = useRouteMatch();
+  return (
+    <div className="preview-bin">
+      <Link to={`${url}/${title.toLowerCase()}`}>
+        <h1 className="title">
+          {title.toUpperCase()}
+        </h1>
+      </Link>
+      <div className="preview">
+        {items
+          .filter((item, index) => index < 4)
+          .map((item) =>
+            <ShopItem key={item.id} item={item} />
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 
 export default ShopPreview;
