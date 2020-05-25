468f928b21021dda516940598b1fa3b5d9e48157
j-gens <jgens2@gmail.com>
2020-05-24 17:53:08 -0700

    Add data for testing front end

---
+++ src/data/shopData.js
@@ -0,0 +1,256 @@
+const SHOP_DATA = {
+  succulents: {
+    id: 1,
+    title: 'Succulents',
+    routeName: 'shop/succulents',
+    items: [
+      {
+        id: 1,
+        name: 'Succulent One',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/s-1.jpg',
+        price: 25,
+      },
+      {
+        id: 2,
+        name: 'Succulent Two',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/s-2.jpg',
+        price: 18,
+      },
+      {
+        id: 3,
+        name: 'Succulent Three',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/s-3.jpg',
+        price: 35,
+      },
+      {
+        id: 4,
+        name: 'Succulent Four',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/s-4.jpg',
+        price: 25,
+      },
+      {
+        id: 5,
+        name: 'Succulent Five',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/s-5.jpg',
+        price: 18,
+      },
+      {
+        id: 6,
+        name: 'Succulent Six',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/s-6.jpg',
+        price: 14,
+      },
+      {
+        id: 7,
+        name: 'Succulent Seven',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/s-7.jpg',
+        price: 18,
+      },
+      {
+        id: 8,
+        name: 'Succulent Eight',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/s-8.jpg',
+        price: 14,
+      },
+      {
+        id: 9,
+        name: 'Succulent Nine',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/s-9.jpg',
+        price: 16,
+      },
+    ],
+  },
+  cacti: {
+    id: 2,
+    title: 'Cacti',
+    routeName: 'shop/cacti',
+    items: [
+      {
+        id: 10,
+        name: 'Cactus One',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/c-1.jpg',
+        price: 220,
+      },
+      {
+        id: 11,
+        name: 'Cactus Two',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/c-2.jpg',
+        price: 280,
+      },
+      {
+        id: 12,
+        name: 'Cactus Three',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/c-3.jpg',
+        price: 110,
+      },
+      {
+        id: 13,
+        name: 'Cactus Four',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/c-4.jpg',
+        price: 160,
+      },
+      {
+        id: 14,
+        name: 'Cactus Five',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/c-5.jpg',
+        price: 160,
+      },
+      {
+        id: 15,
+        name: 'Cactus Six',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/c-6.jpg',
+        price: 160,
+      },
+      {
+        id: 16,
+        name: 'Cactus Seven',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/c-7.jpg',
+        price: 190,
+      },
+      {
+        id: 17,
+        name: 'Cactus Eight',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/c-8.jpg',
+        price: 200,
+      },
+    ],
+  },
+  ferns: {
+    id: 3,
+    title: 'Ferns',
+    routeName: 'shop/ferns',
+    items: [
+      {
+        id: 18,
+        name: 'Fern One',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/f-1.jpg',
+        price: 125,
+      },
+      {
+        id: 19,
+        name: 'Fern Two',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/f-2.jpg',
+        price: 90,
+      },
+      {
+        id: 20,
+        name: 'Fern Three',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/f-3.jpg',
+        price: 90,
+      },
+      {
+        id: 21,
+        name: 'Fern Four',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/f-4.jpg',
+        price: 165,
+      },
+      {
+        id: 22,
+        name: 'Fern Five',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/f-5.jpg',
+        price: 185,
+      },
+    ],
+  },
+  houseplants: {
+    id: 4,
+    title: 'House Plants',
+    routeName: 'shop/houseplants',
+    items: [
+      {
+        id: 23,
+        name: 'House Plant One',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/hp-1.jpg',
+        price: 25,
+      },
+      {
+        id: 24,
+        name: 'House Plant Two',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/hp-2.jpg',
+        price: 20,
+      },
+      {
+        id: 25,
+        name: 'House Plant Three',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/hp-3.jpg',
+        price: 80,
+      },
+      {
+        id: 26,
+        name: 'House Plant Four',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/hp-4.jpg',
+        price: 80,
+      },
+      {
+        id: 27,
+        name: 'House Plant Five',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/hp-5.jpg',
+        price: 45,
+      },
+      {
+        id: 28,
+        name: 'House Plant Six',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/hp-6.jpg',
+        price: 135,
+      },
+      {
+        id: 29,
+        name: 'House Plant Seven',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/hp-7.jpg',
+        price: 20,
+      },
+    ],
+  },
+  flowers: {
+    id: 5,
+    title: 'Flowers',
+    routeName: 'shop/flowers',
+    items: [
+      {
+        id: 30,
+        name: 'Flower One',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/ff-1.jpg',
+        price: 325,
+      },
+      {
+        id: 31,
+        name: 'Flower Two',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/ff-2.jpg',
+        price: 20,
+      },
+      {
+        id: 32,
+        name: 'Flower Three',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/ff-3.jpg',
+        price: 25,
+      },
+      {
+        id: 33,
+        name: 'Flower Four',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/ff-4.jpg',
+        price: 25,
+      },
+      {
+        id: 34,
+        name: 'Flower Five',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/ff-5.jpg',
+        price: 40,
+      },
+      {
+        id: 35,
+        name: 'Flower Six',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/ff-6.jpg',
+        price: 25,
+      },
+      {
+        id: 36,
+        name: 'Flower Seven',
+        imageUrl: 'https://plant-commerce-app.s3-us-west-1.amazonaws.com/ff-7.jpg',
+        price: 35,
+      },
+    ],
+  },
+};
+
+
+export default SHOP_DATA;
