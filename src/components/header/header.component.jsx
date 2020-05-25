e19a0a78ded1e124f14462fdb118ca59e4612754
j-gens <jgens2@gmail.com>
2020-05-24 17:52:45 -0700

    Add name of site to header and title

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,7 +6,7 @@ import { createStructuredSelector } from 'reselect';
 import CartIcon from '../cart-icon/cart-icon.component.jsx';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component.jsx';
 import { auth } from '../../firebase/firebase.utils.js';
-import { currentUserSelector } from '../../redux/user/user.selectors.js';
+import currentUserSelector from '../../redux/user/user.selectors.js';
 import { hiddenCartSelector } from '../../redux/cart/cart.selectors.js';
 
 import './header.styles.css';
@@ -15,7 +15,7 @@ import './header.styles.css';
 const Header = ({ currentUser, isHidden }) => (
   <div className="header-bin">
     <div className="header-logo">
-      <Link to="/">LOGO</Link>
+      <Link to="/">PLANTOLOGY</Link>
     </div>
     <div className="header-options">
       <Link className="option" to="/shop">SHOP</Link>
